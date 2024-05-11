b5779a4d27eb9aa9b2f7b4810bcebad0b47bf6ea
Aman kumar <amank0259@gmail.com>
2024-05-11 18:10:02 +0530

    change UI according to the requirement by adding conditional formating

--- src/Component/SearchPage.jsx
+++ src/Component/SearchPage.jsx
@@ -15,16 +15,16 @@ function SearchPage({ results }) {
                         <div className='container mx-auto rounded-lg shadow-xl bg-gradient-to-r from-white via-white to-green-100 flex flex-row flex-wrap h-48'>
                             <div className='left flex-1 flex flex-col justify-evenly mx-10 flex-wrap'>
                                 <p className='text-lg flex justify-between w-[55%]'>From :
-                                    <span className='' onClick={(e) => setSelectForm(e.target.textContent)}>Tablet1</span>
-                                    <span onClick={(e) => setSelectForm(e.target.textContent)}>Tablet2</span>
+                                    <span className={`cursor-pointer px-2 py-1 text-sm rounded ${selectForm === 'Tablet1' ? 'border-2 border-green-900 shadow shadow-green-400 font-bold' : 'border-dashed border-2 border-zinc-400'}`} onClick={(e) => setSelectForm('Tablet1')}>Tablet1</span>
+                                    <span className={`cursor-pointer px-2 py-1 text-sm rounded ${selectForm === 'Tablet2' ? 'border-2 border-green-900 shadow shadow-green-400' : 'border-dashed border-2 border-zinc-400'}`} onClick={(e) => setSelectForm('Tablet2')}>Tablet2</span>
                                 </p>
                                 <p className='text-lg flex justify-between w-[55%]'>Strength :
-                                    <span onClick={(e) => setSelectStrength(e.target.textContent)}>100mg</span>
-                                    <span onClick={(e) => setSelectStrength(e.target.textContent)}>500mg</span>
+                                    <span className={`cursor-pointer px-2 py-1 text-sm rounded ${selectStrength === '100mg' ? 'border-2 border-green-900 shadow shadow-green-400' : 'border-dashed border-2 border-zinc-400'}`} onClick={(e) => setSelectStrength('100mg')}>100mg</span>
+                                    <span className={`cursor-pointer px-2 py-1 text-sm rounded ${selectStrength === '500mg' ? 'border-2 border-green-900 shadow shadow-green-400' : 'border-dashed border-2 border-zinc-400'}`} onClick={(e) => setSelectStrength('500mg')}>500mg</span>
                                 </p>
                                 <p className='text-lg flex justify-between w-[55%]'>Packaging :
-                                    <span onClick={(e) => setSelectPacking(e.target.textContent)}>5 strips</span>
-                                    <span onClick={(e) => setSelectPacking(e.target.textContent)}>10 strips</span>
+                                    <span className={`cursor-pointer px-2 py-1 text-sm rounded ${selectPacking === '5 strips' ? 'border-2 border-green-900 shadow shadow-green-400' : 'border-dashed border-2 border-zinc-400'}`} onClick={(e) => setSelectPacking('5 strips')}>5 strips</span>
+                                    <span className={`cursor-pointer px-2 py-1 text-sm rounded ${selectPacking === '10 strips' ? 'border-2 border-green-900 shadow shadow-green-400' : 'border-dashed border-2 border-zinc-400'}`} onClick={(e) => setSelectPacking('10 strips')}>10 strips</span>
                                 </p>
                             </div>
                             <div className='center flex-1 flex flex-col text-center mx-10 flex-wrap'>
@@ -37,6 +37,7 @@ function SearchPage({ results }) {
                                 <h1 className='text-center text-4xl font-bold'>From $80</h1>
                             </div>
                         </div>
+
                     </div>
                 </div>) :
                 (
